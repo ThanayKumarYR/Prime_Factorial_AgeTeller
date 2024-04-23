21cb647160584be47b7ece600fb8eba014e92719
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-04-23 12:28:00 +0530

    init

---
+++ AgeTeller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Factorial_AgeTeller
+{
+
+    public class Demo
+    {
+        public int i;
+        static Demo()
+        {
+            Console.WriteLine("Static constructor is initialized");
+        }
+        public Demo()
+        { 
+            Console.WriteLine("Defualt constructor is initialized");
+        }
+        public Demo(int x)
+        {
+            i = x;
+            Console.WriteLine("Parameterized Constructor is initialized");
+        }
+    }
+    class AgeTeller
+    {
+        private int age = 34;
+        public string name = "Thanay Kumar Y R";
+        public int tellage() { return age; }
+
+        static void Main() 
+        
+        {
+        
+            Console.WriteLine("Executing main function");
+            Demo obg = new Demo();
+            Demo obg1 = new Demo(10);
+            Console.WriteLine($"Value of data member is: {obg1.i}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Factorial_AgeTeller
+{
+    internal class Program
+
+    {
+
+        static long Fact(int num) 
+        {
+            if (num == 0) return 1;
+            else return num*Fact(num-1);
+        }
+
+        static void Isprime(int num) 
+        {
+            if (num == 0 || num == 1)
+            {
+                Console.WriteLine($"{num} is neither prime nor const.");
+                return;
+            }else if (num == 2)
+            {
+                Console.WriteLine($"{num} is a prime number.");
+                return;
+            }else 
+            {
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine($"{num} is not a prime number.");
+                    return;
+                }
+                else 
+                {
+                    for (int i = 3; i <= Math.Sqrt(num); i += 2)
+                    {
+                        if (num % i == 0)
+                        {
+                            Console.WriteLine($"{num} is not a prime number.");
+                            return;
+                        }
+                    }
+                    Console.WriteLine($"{num} is a prime number");
+                }
+            }
+        }
+        static void Main(string[] args)
+
+        {
+            //Console.WriteLine(fact(30));
+            //isprime(97);
+
+            AgeTeller obg = new AgeTeller();
+            Console.WriteLine($"Hello {obg.name} your age is {obg.tellage()}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
